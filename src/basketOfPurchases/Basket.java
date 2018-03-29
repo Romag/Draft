f96fea5a63006f0a11711421cef217f7bda1225e
Romag <rmagerramov11@gmail.com>
2018-03-29 08:28:18 +0300

    Second Commit.

---
+++ src/basketOfPurchases/Basket.java
@@ -0,0 +1,13 @@
+package basketOfPurchases;
+
+public class Basket {
+
+	public static void main(String[] args) {
+
+	}
+
+	int[] minimalBasketPrice(int maxPrice, int[] vendorsDelivery, int[][] vendorsProducts) {
+
+		return new int[0];
+	}
+}
